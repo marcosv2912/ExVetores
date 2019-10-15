c118bc33c9a4fca4695f3d3ace47b429aa6ec61c
Marcos Paixao <marcosv2912@gmail.com>
2019-10-15 08:45:23 -0300

    Exercicio Vetores

---
+++ ExVetores/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ExVetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos Quartos serão alugados: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Rooms[] vect = new Rooms[10];
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Aluguel #{i}:");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vect[quarto] = new Rooms(nome, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados: ");
+            for (int i = 0; i < vect.Length; i++)
+            {
+                Console.WriteLine(i +": "+vect[i]);
+            }
+        }
+    }
+}
---
+++ ExVetores/Rooms.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExVetores
+{
+    class Rooms
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Rooms(string nome, string email)
+        {
+            Name = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Email;
+        }
+    }
+}
